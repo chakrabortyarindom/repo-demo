38c3b499b8c43ee7856ca57e67bdcc3c775cc070
chakrabortyarindom <arindomc628@gmail.com>
2025-08-23 23:21:16 +0600

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,14 @@
+public class datatypes {
+    public static void main(String[] args){
+        int mynum=5;
+        float myflotnum=3.92f;
+        char myletter = 'd';
+        boolean mybool= true;
+        String mytext= "hello";
+        System.out.println(mynum);
+        System.out.println(myflotnum);
+        System.out.println(myletter);
+        System.out.println(mybool);
+        System.out.println(mytext);
+    }
+}
